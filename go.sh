d75711f817ac4b7449b0fa6227c63da174edd7fe
Ron Kurr <kurr@kurron.org>
2016-01-06 15:19:21 +0000

    Created a convenience script to test out the build

---
+++ go.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export ORG_GRADLE_PROJECT_family=Alpha
+export ORG_GRADLE_PROJECT_containerName=Bravo
+export ORG_GRADLE_PROJECT_registry=Charlie
+export ORG_GRADLE_PROJECT_imageName=Delta
+export ORG_GRADLE_PROJECT_imageTag=Echo
+export ORG_GRADLE_PROJECT_clusterName=Foxtrot
+export ORG_GRADLE_PROJECT_serviceName=Golf
+export ORG_GRADLE_PROJECT_loadBalancerName=Hotel
+export ORG_GRADLE_PROJECT_clientToken=India
+export ORG_GRADLE_PROJECT_ecsInstanceRole=Juliett
+
+rm -rf build 
+gradle
+cat build/definitions/*
